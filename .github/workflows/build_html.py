1ba03cedda4e30b5cbb8be5fe2e6b3bfc06a59a3
Giuseppe Tutino <giuseppetutino1@gmail.com>
2024-11-05 19:15:43 +0100

    #19 GH Pages workflow implementation

--- .github/workflows/build_html.py
+++ .github/workflows/build_html.py
@@ -1,10 +1,13 @@
 import re
 import os
 import glob
+import shutil
 
 def build_link(path):
     link = "https://raw.githubusercontent.com/techminds-unipd/docs/refs/heads/main/{}".format(os.path.abspath(path).replace(repo_dir, ""))
-    return "<li><a href=\"{}\">{}</a><i class=\"fa-solid fa-arrow-up-right-from-square\"></i></li>\n".format(link, os.path.basename(path).replace(".pdf",""))
+    name = os.path.basename(path).replace(".pdf","")
+    name = name[0].upper() + name[1:]
+    return "<li><a href=\"{}\" target=\"_blank\" >{}</a> <i class=\"fa-solid fa-arrow-up-right-from-square\"></i></li>\n".format(link, name)
 
 repo_dir = os.environ['GITHUB_WORKSPACE']
 
@@ -16,11 +19,19 @@ pdfs = glob.glob("{}/build/**/*.pdf".format(repo_dir), recursive=True)
 
 for i in re.findall("{.*}", content):
     lists = ""
+    add = []
     for pdf in pdfs:
         if i[1:-1] in pdf:
-            lists = lists + build_link(pdf)
+            add.append(pdf)
+    add.sort(reverse=True)
+    for pdf in add:
+        lists = lists + build_link(pdf)
     content = content.replace(i, "\n{}".format(lists))
 
 f = open("{}/build/index.html".format(repo_dir), "w")
 f.write(content)
 f.close()
+
+# CSS and Logo
+shutil.copyfile("{}/template/style.css".format(repo_dir), "{}/build/style.css".format(repo_dir))
+shutil.copyfile("{}/assets/logo.png".format(repo_dir), "{}/build/logo.png".format(repo_dir))
