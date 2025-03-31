ea499b0f06c414c3ec82f1a054b2c2049dc0c376
cvallott <caterina.vallotto@studenti.unipd.it>
2025-03-31 15:11:19 +0200

    fix glossario

--- .github/workflows/check_glossario.py
+++ .github/workflows/check_glossario.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
         sources = glob.glob("{}/**/*.typ".format(REPO_DIR), recursive=True)
 
         # Blacklist
-        blacklist = ["template/", "glossario", "candidatura/", "/include/", "/verbali/", "lettera-di-presentazione-RTB.typ"]
+        blacklist = ["template/", "glossario", "candidatura/", "/include/", "/verbali/", "lettera-di-presentazione-RTB.typ", "lettera-di-presentazione-PB.typ"]
         for source in sources:
             if not any([string in source for string in blacklist]):
                 sources_to_check.append(source)
